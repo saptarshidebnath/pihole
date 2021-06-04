dcfcaa89b7bcb4bc9e21a9c1c770407e0a35d28f
Saptarshi Debnath <github@saptarshidebnath.com>
2021-06-04 06:00:20 +0000

    Changes for auto restart and setup

--- setup.sh
+++ setup.sh
@@ -4,16 +4,29 @@
 __backup_config=./pihole/conf
 __target_config=./pihole/vol/etc/pihole
 
-echo
-echo Removing previous stack && docker-compose down
 
-echo Resetting addlist and custom dns mapping.
-rm -rf ${__target_config}
-mkdir ${__target_config}
-cp ${__backup_config}/adlists.list.bak ${__target_config}/adlists.list
-cp ${__backup_config}/custom.list.bak ${__target_config}/custom.list
-cp ${__backup_config}/custom-blocklist.list.bak ${__target_config}/custom-blocklist.list
+VAR="$1"
+
+case $VAR in
+
+  "-r")
+    echo
+    echo Removing previous stack && docker-compose down
+    echo Resetting addlist and custom dns mapping.
+    sudo rm -rf ${__target_config}
+    mkdir ${__target_config}
+    cp ${__backup_config}/adlists.list.bak ${__target_config}/adlists.list
+    cp ${__backup_config}/custom.list.bak ${__target_config}/custom.list
+    cp ${__backup_config}/custom-blocklist.list.bak ${__target_config}/custom-blocklist.list
+    ;;
+
+  *)
+    echo '(Re)Starting containers'
+    ;;
+esac
+
 echo
 echo Setting up new stack && docker-compose up -d --build
 echo Running post setup code && docker-compose exec pihole /usr/bin/postsetup.sh
 
+docker-compose logs --follow
