136e2d0cff34ce2486eb2ca5dc0ff0e0255a684a
Saptarshi Debnath <7835253+saptarshidebnath@users.noreply.github.com>
2022-02-12 22:05:45 -0500

    Updated Script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 
 
 __backup_config=./pihole/conf
-__target_config=./pihole/vol/etc/pihole
+__pihole_vol=./pihole/vol
+__target_config=${__pihole_vol}/etc/pihole
 
 
 VAR="$1"
@@ -11,10 +12,10 @@ case $VAR in
 
   "-r")
     echo
-    echo Removing previous stack && docker-compose down
+    echo Removing previous stack && docker-compose down -v 
     echo Resetting addlist and custom dns mapping.
-    sudo rm -rf ${__target_config}
-    mkdir ${__target_config}
+    sudo -rf ${__pihole_vol}
+    mkdir -p ${__target_config}
     cp ${__backup_config}/adlists.list.bak ${__target_config}/adlists.list
     cp ${__backup_config}/custom.list.bak ${__target_config}/custom.list
     cp ${__backup_config}/custom-blocklist.list.bak ${__target_config}/custom-blocklist.list
